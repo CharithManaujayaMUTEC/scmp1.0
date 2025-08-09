367e5a71d057f934f21839c6651efc9706ae520d
Jayani Perera <pererajayani27@gmail.com>
2025-08-09 23:40:54 +0530

    Change

--- app/storage-request-receipt/page.tsx
+++ app/storage-request-receipt/page.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import DashboardLayout from "../components/DashboardLayout";
 import QRCode from "react-qr-code";
 import { useSearchParams, useRouter } from "next/navigation";
+export const dynamic = 'force-dynamic'
 
 type ReceiptData = {
   requestId: string;
